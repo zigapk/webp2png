6392421205707baeb70e9c1cebfe510592007632
Ziga Patacko Koderman <ziga.patacko@protonmail.com>
2017-05-07 20:07:09 +0200

    add code and binary

---
+++ webp2png.py
@@ -0,0 +1,15 @@
+from os import listdir
+from os.path import isfile, join
+import sys
+import os
+
+path = sys.argv[1]
+dwebppath = "dwebp"
+
+files = [f for f in listdir(path) if isfile(join(path, f))]
+
+for f in files:
+	if f[-4:].lower() == 'webp':
+		os.system(dwebppath + ' -o \"' + path + f[:-5] + '.png\" \"' + path + f + '\"')
+
+print("Done")
